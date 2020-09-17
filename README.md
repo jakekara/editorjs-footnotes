2b36b0edd8e64248c377dfcc517f36db3e99142e
Jake <jakekara@users.noreply.github.com>
2020-09-17 10:21:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boilerplate JS project
+
+> A boilerplate JS project for a browser environment.
+
+## Typescript
+
+### Importing non-code assets
+
+Extend custom.d.ts to add new types. See [the ts docs](https://webpack.js.org/guides/typescript/#importing-other-assets) for info on importing non-code assets.
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
