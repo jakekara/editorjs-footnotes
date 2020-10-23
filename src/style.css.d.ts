11a49604c958821162243cb998bffad2c3d00392
Jake Kara <jake@jakekara.com>
2020-10-23 12:13:27 -0400

    Update styles

--- src/style.css.d.ts
+++ src/style.css.d.ts
@@ -1,17 +1,19 @@
 declare namespace StyleCssNamespace {
   export interface IStyleCss {
-    barLabel: string;
-    contentArea: string;
-    embedCode: string;
-    embedPreview: string;
-    footnoteBlock: string;
-    textInput: string;
+    barLabel: string
+    contentArea: string
+    embedCode: string
+    embedCodeLabel: string
+    embedPreview: string
+    footnoteBlock: string
+    idField: string
+    textInput: string
   }
 }
 
 declare const StyleCssModule: StyleCssNamespace.IStyleCss & {
   /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
-  locals: StyleCssNamespace.IStyleCss;
-};
+  locals: StyleCssNamespace.IStyleCss
+}
 
-export = StyleCssModule;
+export = StyleCssModule
