1840ffe52beec0e8d8eb3a5fccdc6b81d30b12b7
Jake Kara <jake@jakekara.com>
2020-10-23 12:12:46 -0400

    Make embed code field permanent.

--- src/Footnote.ts
+++ src/Footnote.ts
@@ -1,208 +1,152 @@
-import Paragraph from "@editorjs/paragraph";
-import { FootnoteMaker } from "./FootnoteMaker";
-import { generateID } from "./generateID";
-import { API, BlockToolData, EditorConfig } from "@editorjs/editorjs";
-import styles from "./style.css";
+import Paragraph from '@editorjs/paragraph'
+import { FootnoteMaker } from './FootnoteMaker'
+import { generateID } from './generateID'
+import { API, BlockToolData, EditorConfig } from '@editorjs/editorjs'
+import styles from './style.css'
 
 interface TuneSetting {
-  name: string;
-  icon: string;
-  active: boolean;
-  handleClick: () => void;
+  name: string
+  icon: string
+  active: boolean
+  handleClick: () => void
 }
 
 export class Footnote extends Paragraph {
   constructor(opts: { data: BlockToolData; config: EditorConfig; api: API }) {
-    const { data, config, api } = opts;
-    super({ data, config, api });
+    const { data, config, api } = opts
+    super({ data, config, api })
 
     // if no ID is set, set one
     if (!data.id) {
-      const initialID = generateID();
-      this._data.id = initialID;
+      const initialID = generateID()
+      this._data.id = initialID
     }
 
     // if not text is set, set it
     if (!data.text) {
-      this._data.text = "";
+      this._data.text = ''
     }
 
-    this.enableEmbedCode = false;
-
-    this.toggleEmbedCode = this.toggleEnableEmbedCode.bind(this);
-    this.save = this.save.bind(this);
-    this.render = this.render.bind(this);
-    this.renderSettings = this.renderSettings.bind(this);
-    this.renderEmbedCode = this.renderEmbedCode.bind(this);
-
-    this.settings = [
-      {
-        name: "addEmbedCode",
-        icon: `<>`,
-        active: false,
-        handleClick: this.toggleEmbedCode.bind(this),
-      },
-    ];
+    this.save = this.save.bind(this)
+    this.render = this.render.bind(this)
+    this.renderEmbedCode = this.renderEmbedCode.bind(this)
   }
 
-  renderEmbedCode() {
-    let preview = this.wrapper.querySelector("." + styles.embedPreview);
-    let embedCode = this.wrapper.querySelector("." + styles.embedCode).value;
+  renderEmbedCode(e?: any) {
+    e?.preventDefault();
+    let embedCodeLabel = this.wrapper.querySelector('.' + styles.embedCodeLabel)
+    let embedCode = this.wrapper.querySelector('.' + styles.embedCode)
+    let embedCodePreview = this.wrapper.querySelector('.' + styles.embedPreview)
 
-    console.log("renderEmbedCode", this.enableEmbedCode);
-    if (!this.enableEmbedCode) {
-      console.log("Removing embed code preview");
-      preview.remove();
-
-      // // remove preview
-      // if (preview) {
-      //   preview.remove();
-      // }
+    if (!embedCodeLabel) {
+      embedCodeLabel = document.createElement('div')
+      embedCodeLabel.classList.add(styles.embedCodeLabel)
+      embedCodeLabel.classList.add(styles.barLabel)
+      embedCodeLabel.innerHTML = 'Embed code'
+      this.wrapper.appendChild(embedCodeLabel)
     }
 
-    if (!preview) {
-      preview = document.createElement("div");
-      preview.classList.add(styles.embedPreview);
-      this.wrapper.appendChild(preview);
+    if (!embedCode) {
+      embedCode = document.createElement('textarea')
+      embedCode.value = this.data.embedCode || ""
+      embedCode.classList.add(styles.embedCode)
+      embedCode.addEventListener('change', this.renderEmbedCode)
+      embedCode.addEventListener('keyup', this.renderEmbedCode)
+      this.wrapper.appendChild(embedCode)
     }
 
-    preview.innerHTML = embedCode;
-  }
+    if (!embedCodePreview) {
+      embedCodePreview = document.createElement('div')
+      embedCodePreview.classList.add(styles.embedPreview)
 
-  toggleEnableEmbedCode() {
-    this.enableEmbedCode = !this.enableEmbedCode;
-
-    console.log("Enable embed code", this.enableEmbedCode);
-    let embedCode = this.wrapper.querySelector("." + styles.embedCode);
-    let embedCodeLabel = this.wrapper.querySelector(".embed-code-label");
-    let embedCodePreview = this.wrapper.querySelector(
-      "." + styles.embedPreview
-    );
-
-    if (this.enableEmbedCode) {
-      if (embedCode) {
-        return;
-      }
-
-      embedCodeLabel = document.createElement("div");
-      embedCodeLabel.classList.add("embed-code-label");
-      embedCodeLabel.classList.add(styles.barLabel);
-      embedCodeLabel.innerHTML = "Embed code";
-      this.wrapper.appendChild(embedCodeLabel);
-
-      embedCode = document.createElement("textarea");
-      embedCode.classList.add(styles.embedCode);
-      this.wrapper.appendChild(embedCode);
-      embedCode.addEventListener("change", this.renderEmbedCode);
-      embedCode.addEventListener("keyup", this.renderEmbedCode);
-    } else {
-      embedCodeLabel.remove();
-      embedCode.remove();
-      if (embedCodePreview) {
-        embedCodePreview.remove();
-      }
+      embedCodePreview.innerHTML = embedCode.value
+      this.wrapper.appendChild(embedCodePreview)
     }
+
+    embedCodePreview.innerHTML = embedCode.value
+
   }
 
   static get sanitize() {
     return {
       id: false,
+      label: false, // TODO - Is this right?
       text: { i: true, a: true, b: true },
       embedCode: true,
-    };
+    }
   }
 
   save(blockContent: BlockToolData) {
-    const content = blockContent.querySelector("." + styles.contentArea);
-    const text = content ? content.innerHTML : "";
+    const content = blockContent.querySelector('.' + styles.contentArea)
+    const text = content ? content.innerHTML : ''
 
     const id = blockContent
-      // .querySelector("." + styles.metaBar)
-      .querySelector(".id-field")
-      .getAttribute("data-id");
+      .querySelector('.' + styles.idField)
+      .getAttribute('data-id')
 
-    const label = blockContent.querySelector(".label-field").textContent;
+    const label = blockContent.querySelector('.label-field').textContent
 
-    const embedCode = blockContent.querySelector("." + styles.embedCode)
-      ? blockContent.querySelector("." + styles.embedCode).value
-      : undefined;
+    const embedCode = blockContent.querySelector('.' + styles.embedCode).value
 
     const ret = {
       id,
       label,
       text,
       embedCode,
-    };
+    }
 
-    return ret;
-  }
+    console.log("Saving", ret);
 
-  renderSettings() {
-    const wrapper = document.createElement("div");
-
-    this.settings.forEach((tune: TuneSetting) => {
-      let button = document.createElement("div");
-
-      button.classList.add("cdx-settings-button");
-      button.innerHTML = tune.icon;
-      wrapper.appendChild(button);
-
-      if (tune.active) {
-        button.classList.add("cdx-settings-button--active");
-      }
-
-      button.addEventListener("click", tune.handleClick);
-    });
-
-    return wrapper;
+    return ret
   }
 
   render() {
-    const wrapper = document.createElement("div");
-    wrapper.classList.add(styles.footnoteBlock);
+    const wrapper = document.createElement('div')
+    wrapper.classList.add(styles.footnoteBlock)
 
-    const metaBar = document.createElement("div");
-    // metaBar.classList.add(styles.metaBar);
-    metaBar.classList.add("id-field");
-    metaBar.classList.add(styles.barLabel);
-    metaBar.setAttribute("data-id", this.data.id);
-    metaBar.innerHTML = "[ #" + this.data.id + " ]";
-    wrapper.appendChild(metaBar);
+    const idBar = document.createElement('div')
+    idBar.classList.add(styles.idField)
+    idBar.classList.add(styles.barLabel)
+    idBar.setAttribute('data-id', this.data.id)
+    idBar.innerHTML = '[ #' + this.data.id + ' ]'
+    wrapper.appendChild(idBar)
 
-    const labelBar = document.createElement("div");
-    // labelBar.classList.add(styles.metaBar);
-    labelBar.classList.add(styles.barLabel);
-    const label = document.createElement("div");
-
-    label.innerHTML = "footnote label";
-    const labelInput = document.createElement("div");
-    labelInput.setAttribute("contentEditable", "true");
-    labelInput.classList.add("label-field");
+    // const labelBar = document.createElement('div')
+    // labelBar.classList.add(styles.barLabel)
+    const label = document.createElement('div')
+    label.innerHTML = 'footnote label'
+    label.classList.add(styles.barLabel);
+    const labelInput = document.createElement('div')
+    labelInput.setAttribute('contentEditable', 'true')
+    labelInput.classList.add('label-field')
     // labelInput.classList.add(styles.flexFill);
-    labelInput.classList.add(styles.textInput);
-    labelBar.appendChild(label);
-    labelBar.appendChild(labelInput);
-    wrapper.appendChild(labelBar);
+    labelInput.classList.add(styles.textInput)
+    labelInput.innerHTML = this.data.label || ""
+    wrapper.appendChild(label)
+    wrapper.appendChild(labelInput)
+    // wrapper.appendChild(labelBar)
 
-    const contentLabel = document.createElement("div");
-    contentLabel.classList.add(styles.barLabel);
-    contentLabel.innerHTML = "footnote body";
-    wrapper.appendChild(contentLabel);
+    const contentLabel = document.createElement('div')
+    contentLabel.classList.add(styles.barLabel)
+    contentLabel.innerHTML = 'footnote body'
+    wrapper.appendChild(contentLabel)
 
-    const contentArea = document.createElement("div");
-    contentArea.classList.add(styles.contentArea);
-    contentArea.classList.add(styles.textInput);
+    const contentArea = document.createElement('div')
+    contentArea.classList.add(styles.contentArea)
+    contentArea.classList.add(styles.textInput)
 
-    contentArea.classList.add("ce-paragraph");
-    contentArea.innerHTML = this.data.text;
-    contentArea.contentEditable = "true";
-    wrapper.appendChild(contentArea);
+    contentArea.classList.add('ce-paragraph')
+    contentArea.innerHTML = this.data.text
+    contentArea.contentEditable = 'true'
+    wrapper.appendChild(contentArea)
 
-    contentArea.addEventListener("keyup", this.onKeyUp);
+    contentArea.addEventListener('keyup', this.onKeyUp)
 
-    this.wrapper = wrapper;
+    this.wrapper = wrapper
 
-    return wrapper;
+    this.renderEmbedCode()
+
+    return wrapper
   }
 
   /**
@@ -212,10 +156,10 @@ export class Footnote extends Paragraph {
    */
   static get toolbox() {
     return {
-      icon: "F",
-      title: "Footnote",
-    };
+      icon: 'F',
+      title: 'Footnote',
+    }
   }
 }
 
-console.log("Defined class", FootnoteMaker);
+console.log('Defined class', FootnoteMaker)
